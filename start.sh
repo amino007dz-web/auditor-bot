5da6cbf9a17b76e4679d0ceb09d7d5f77329faa5
DefiAudit <salahkeven2@gmail.com>
2026-07-19 14:30:03 +0200

    Use WEB_CONCURRENCY env var for workers count instead of hardcoding 2

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ fi
 
 exec gunicorn web_ui:app \
     --bind 0.0.0.0:${PORT:-5000} \
-    --workers 2 \
+    --workers ${WEB_CONCURRENCY:-2} \
     --worker-class gevent \
     --worker-connections 100 \
     --timeout 300 \
